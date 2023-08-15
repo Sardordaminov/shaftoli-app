c7fe0ff7cf511e9059dfdb475c6e4b382b412ba5
Sardor Daminov <sardorbe221007@gmail.com>
2023-08-15 20:20:46 +0500

    salom

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { NavbarMainStyles } from "./navbar.styles";
+import "./navbar.css";
+import { BsSearch } from 'react-icons/bs'
 
 export default function navbar() {
   return (
     <>
-      <NavbarMainStyles.Navbar></NavbarMainStyles.Navbar>
+      <div className="navbar-main">
+        <h1 className="navbar-main-title">SHAFTOLI MARKET</h1>
+        <div className="navbar-main-searchbox">
+          <input
+            className="navbar-main-search"
+            type="search"
+            placeholder="Search a product"
+            id="search"
+          />
+          <label className="search-btn" htmlFor="search"><BsSearch /></label>
+        </div>
+        <div className="reg-btns">
+          <button className="login btn">Log in</button>
+          <button className="signup btn">Sign up</button>
+        </div>
+      </div>
     </>
   );
 }
