2c2bf02d3a199dce93c21de5e369de426980d0a9
Sardor Daminov <sardorbe221007@gmail.com>
2023-09-04 20:34:22 +0500

    salom

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 const Signup = () => {
   document.title = "Sign up"
 
+
   return (
     <div className='signup-page'>
       <div className='signup'>
@@ -12,15 +13,16 @@ const Signup = () => {
         <div className='inputs'>
           <div className="input">
             <label htmlFor="number-input">Phone Number:</label>
+            <p className='number-region'>+998</p>
             <input type="number" className='number-input' placeholder='00 000-00-00' />
           </div>
           <div className="input">
             <label htmlFor="password">Password:</label>
-            <input type="password" className='password' />
+            <input type="password" placeholder='Password' className='password' />
           </div>
           <div className="input">
             <label htmlFor="password">Confirm Password:</label>
-            <input type="password" className='password confirm' />
+            <input type="password" placeholder='Confirm Password' className='password confirm' />
           </div>
           <button type='submit' className='signup-btn'>Sign up</button>
         </div>
