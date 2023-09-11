fbfb0fe6e344d32d984443d247bc3c16ea236d11
Sardor Daminov <sardorbe221007@gmail.com>
2023-09-11 19:04:12 +0500

    salom

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./signup.css"
 import { Link } from 'react-router-dom'
 
 const Signup = () => {
   document.title = "Sign up"
 
+  const [phoneNumber, setPhoneNumber] = useState({
+    number: '',
+    inspection: false
+  }); // Initialize phoneNumber state
+
+  // Function to handle changes in the number-input field
+  const PhoneNumber = (e) => {
+    const value = e.target.value;
+    // console.log(`Number Input Length: ${value.length}`);
+    if (value.length <= 0) {
+      setPhoneNumber({
+        number: "Required",
+        inspection: true
+      })
+    } else if (value.length === 9) {
+      setPhoneNumber('')
+    } else if (value.length > 9 || value.length < 9) {
+      setPhoneNumber('Must be a phone number in Uzbekistan')
+    }
+  };
 
   return (
     <div className='signup-page'>
@@ -14,7 +34,8 @@ const Signup = () => {
           <div className="input">
             <label htmlFor="number-input">Phone Number:</label>
             <p className='number-region'>+998</p>
-            <input type="number" className='number-input' placeholder='00 000-00-00' />
+            <input type="number" className='number-input' placeholder='00 000-00-00' onFocus={PhoneNumber} onChange={PhoneNumber} />
+            <h4 className='number-alert'>{phoneNumber}</h4>
           </div>
           <div className="input">
             <label htmlFor="password">Password:</label>
