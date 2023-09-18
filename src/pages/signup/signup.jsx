b0613367f2cfabe893d017eabf4127187fba7619
Sardor Daminov <sardorbe221007@gmail.com>
2023-09-18 19:41:49 +0500

    salom

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -5,26 +5,33 @@ import { Link } from 'react-router-dom'
 const Signup = () => {
   document.title = "Sign up"
 
-  const [phoneNumber, setPhoneNumber] = useState({
-    number: '',
-    inspection: false
-  }); // Initialize phoneNumber state
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState(""); // Initialize phoneNumber state
 
   // Function to handle changes in the number-input field
-  const PhoneNumber = (e) => {
+  const phonenumber = (e) => {
     const value = e.target.value;
     // console.log(`Number Input Length: ${value.length}`);
     if (value.length <= 0) {
-      setPhoneNumber({
-        number: "Required",
-        inspection: true
-      })
+      setPhoneNumber("Required")
     } else if (value.length === 9) {
       setPhoneNumber('')
     } else if (value.length > 9 || value.length < 9) {
       setPhoneNumber('Must be a phone number in Uzbekistan')
     }
   };
+  const Password = (e) => {
+    const value = e.target.value;
+    // console.log(`Number Input Length: ${value.length}`);
+    if (value.length <= 0) {
+      setPassword("Required")
+    } else if (value.length === 9) {
+      setPassword('')
+    } else if (value.length > 9 || value.length < 9) {
+      setPassword('Must be a phone number in Uzbekistan')
+    }
+  };
+  
 
   return (
     <div className='signup-page'>
@@ -34,12 +41,13 @@ const Signup = () => {
           <div className="input">
             <label htmlFor="number-input">Phone Number:</label>
             <p className='number-region'>+998</p>
-            <input type="number" className='number-input' placeholder='00 000-00-00' onFocus={PhoneNumber} onChange={PhoneNumber} />
+            <input type="number" className='number-input' placeholder='00 000-00-00' onFocus={phonenumber} onChange={phonenumber} />
             <h4 className='number-alert'>{phoneNumber}</h4>
           </div>
           <div className="input">
             <label htmlFor="password">Password:</label>
-            <input type="password" placeholder='Password' className='password' />
+            <input type="password" placeholder='Password' className='password' onFocus={Password} onChange={Password}/>
+            <h4 className='password-alert'>{password}</h4>
           </div>
           <div className="input">
             <label htmlFor="password">Confirm Password:</label>
